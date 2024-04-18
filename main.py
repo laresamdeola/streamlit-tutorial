c0bc6dd658e43b4f1206c31448971dae274068a8
laresamdeola <laresamdeola@gmail.com>
2024-04-18 07:54:11 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import math
+
+# df = pd.DataFrame({
+#     'first column': [1, 2, 3, 4, 5],
+#     'second column': ['A', 'B', 'C', 'D', 'E']
+# })
+
+# df
+
+name_of_table = 'Random Data'
+# st.write(name_of_table)
+
+df = pd.DataFrame({
+    'age': [10, 20, 30],
+    'names': ['Lare', 'Chika', 'Melody']
+})
+
+# st.write(df)
+
+# Using the dataframe
+
+# st.dataframe(df.style.highlight_max(axis=0))
+
+# Using a static table
+
+# st.table(df)
+
+# Plots
+
+# Line Plots
+
+# st.line_chart(df)
+
+chart_data = pd.DataFrame(
+    ([10, 20, 30], [15, 25, 35], [12, 22, 32]), 
+    columns=['a', 'b', 'c']
+    )
+
+# st.line_chart(chart_data)
+
+# Maps
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon']
+)
+
+# st.map(map_data, size=0.05)
+
+# Widgets
+
+st.write('Widgets')
+
+x = st.slider('x')
+st.write(x, 'squared is', math.pow(x, 2))
