1e57c37f7d8a3eef7d0fba440d587a8c067db267
laresamdeola <laresamdeola@gmail.com>
2024-04-19 09:49:11 +0100

    widgets, text input, checkboxes, selectbox, sidebar, layout options and progress

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pandas as pd
 import numpy as np
 import math
 
-# df = pd.DataFrame({
-#     'first column': [1, 2, 3, 4, 5],
-#     'second column': ['A', 'B', 'C', 'D', 'E']
-# })
+df_one = pd.DataFrame({
+    'first column': [1, 2, 3, 4, 5],
+    'second column': ['A', 'B', 'C', 'D', 'E']
+})
 
 # df
 
@@ -64,3 +64,61 @@ map_data = pd.DataFrame(
 # st.session_state.age
 
 # Checkboxes
+
+# if st.checkbox('Show Dataframe'):
+#     chart_data_2 = pd.DataFrame(
+#         np.random.randn(20, 3),
+#         columns=['a', 'b', 'c']
+#     )
+
+#     chart_data_2
+
+# Selectbox
+
+# option = st.selectbox(
+#     'which number do you like best?',
+#     df_one['first column']
+# )
+
+# 'You selected: ', option
+
+# Sidebar
+
+# add_selectbox = st.sidebar.selectbox(
+#     'How would you like to be contacted?',
+#     ('Email', 'Home', 'Mobile')
+# )
+
+# # Add a slider to the sidebar
+# add_slider = st.sidebar.slider(
+#     'Select a range of values',
+#     0.0, 100.0, (25.0, 75.0)
+# )
+
+# Other Layout options
+
+# left_column, right_column = st.columns(2)
+# left_column.button('Press me!')
+
+# with right_column:
+#     chosen = st.radio(
+#         'Sorting hat',
+#         ('Gryfindor', 'Ravenclaw', 'Hufflepuff', 'Slytherin')
+#     )
+#     st.write(f'You are in {chosen} house!')
+
+# Show Progress
+
+import time
+
+# 'Starting a long computation'
+
+# latest_iteration = st.empty()
+# bar = st.progress(0)
+
+# for i in range(100):
+#     latest_iteration.text(f'Iteration {i+1}')
+#     bar.progress(i+1)
+#     time.sleep(0.1)
+
+# '... and now we\'re done!'
